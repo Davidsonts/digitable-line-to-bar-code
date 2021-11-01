66cdf7eff05ac8a397e952f7a0eb45b06a52b069
Davidson Santos <davidsonts@outlook.com>
2021-11-01 11:34:41 -0300

    Linha digitável para código mde barras

--- src/DigitableLineToBarCode.php
+++ src/DigitableLineToBarCode.php
@@ -4,8 +4,42 @@ namespace Davidsonts\DigitableLineToBarCode;
 
 class DigitableLineToBarCode
 {
-    public static function toBarCode()
+    public static function toBarCode($digitableLine)
     {
-        echo "toBarCode";
+        strlen($digitableLine) == 44 or die('Digitable line must have 44 characters');
+        is_numeric($digitableLine) or die('Digitable line must be numeric');
+        
+        $bar_code = [];
+        for ($i=0; $i < 4; $i++) { 
+            $number = substr($digitableLine, 0, 11);
+            $arr = str_split($number);
+            $calc = self::calc($arr);
+            $bar_code[$i] = $number . $calc;  
+            $digitableLine = str_replace($number, '', $digitableLine);
+        }
+        return implode('', $bar_code);
+    }
+
+    private static function calc($arr)
+    {
+        $soma = [];
+        $multiplicador = 2; 
+        foreach (array_reverse($arr) as $key => $value) {
+            $soma[$key] = $value * $multiplicador;
+            $multiplicador = $multiplicador == 9 ? 2 : ++$multiplicador;
+        }
+
+        $rest = (array_sum($soma)) % 11;
+        $result = 11 - $rest;
+
+        if($result > 9) {
+            $total = 0;
+        } elseif($result == 0)  {
+            $total = $result;
+        } else {
+            $total = $result;
+        }
+
+       return $total;
     }
 }
